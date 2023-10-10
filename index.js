45068bde57fe588fb57a8fab73200d01c8177ef1
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-10 17:19:55 -0400

    started coding the inquirer prompts

--- index.js
+++ index.js
@@ -26,19 +26,79 @@ function startApp() {
           console.log('View All Employees');
           break;
         case 'Add a Department':
-          console.log('Add a Department');
+          addDepartment();
           break;
         case 'Add a Role':
-          console.log('Add a Role');
+          addRole();
           break;
         case 'Add an Employee':
-          console.log('Add an Employee');
+          addEmployee();
           break;
         case 'Update an Employee Role':
           console.log('Update an Employee Role');
           break;
       }
     })
+}
+
+function addDepartment() {
+  inquirer
+   .prompt([
+    {
+      name: 'department',
+      message: 'What is the name of the department'
+    }
+   ]).then((answer) => {
+    console.log(answer);
+   });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: 'role',
+        message: 'What is the name of the role?'
+      },
+      {
+        name: 'salary',
+        message: 'What is the salary of the role?'
+      },
+      {
+        name: 'belongsTo',
+        message: 'Which department does the role belong too?'
+      }
+    ]).then((answer) => {
+      console.log(answer);
+    })
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: 'firstName',
+        message: 'What is the employee\'s first name?'
+      },
+      {
+        name: 'lastName',
+        message: 'What is the employee\'s last name?'
+      },
+      {
+        name: 'role',
+        type: 'list',
+        message: 'What is the employee\'s last name?',
+        choices: roleArray
+      },
+      {
+        name: 'manager',
+        type: 'list',
+        message: 'Who is the employee\'s manager',
+        choices: managerArray
+      }
+    ]).then((answer) => {
+      console.log(answer);
+    });
 }
 
 
